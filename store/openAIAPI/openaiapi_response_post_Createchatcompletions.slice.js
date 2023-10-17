d8cc848e51a6d4e65f8855f480ff1091b9d2f5dd
Crowdbotics <engineering@crowdbotics.com>
2023-10-17 15:32:18 +0000

    Update store.

---
+++ store/openAIAPI/openaiapi_response_post_Createchatcompletions.slice.js
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const openaiapi_post_v1_chat_completions_create = createAsyncThunk(
+  "openaiapi_response_post_Createchatcompletions/openaiapi_post_v1_chat_completions_create",
+  async payload => {
+    const response = await apiService.openaiapi_post_v1_chat_completions_create(
+      payload
+    )
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const openaiapi_response_post_CreatechatcompletionsSlice = createSlice({
+  name: "openaiapi_response_post_Createchatcompletions",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(
+        openaiapi_post_v1_chat_completions_create.pending,
+        (state, action) => {
+          if (state.api.loading === "idle") {
+            state.api.loading = "pending"
+          }
+        }
+      )
+      .addCase(
+        openaiapi_post_v1_chat_completions_create.fulfilled,
+        (state, action) => {
+          if (state.api.loading === "pending") {
+            state.entities.push(action.payload)
+            state.api.loading = "idle"
+          }
+        }
+      )
+      .addCase(
+        openaiapi_post_v1_chat_completions_create.rejected,
+        (state, action) => {
+          if (state.api.loading === "pending") {
+            state.api.error = action.error
+            state.api.loading = "idle"
+          }
+        }
+      )
+  }
+})
+export default {
+  openaiapi_post_v1_chat_completions_create,
+  slice: openaiapi_response_post_CreatechatcompletionsSlice
+}
